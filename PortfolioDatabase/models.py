474f272ca028d580b9ea9392b59877fd9f6047f3
Jesus Otteson <jesusotteson@mail.weber.edu>
2020-09-29 09:22:53 -0600

    Committing Database Items

--- PortfolioDatabase/models.py
+++ PortfolioDatabase/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Hobby(models.Model):
+
+    def __str__(self):
+        return self.hobby_name
+
     hobby_name = models.CharField(max_length=200)
     hobby_description = models.CharField(max_length=200)
 
--- PortfolioDatabase/views.py
+++ PortfolioDatabase/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 def Home(request):
     return HttpResponse('Hey! My name is Jp.\nI like programming, video games, and cats.\nIm vibing.')
 
+
 def Hobbies(request):
     return
+
 
 def Portfolio(request):
     return
 
+
 def Contact(request):
-    return
+    return HttpResponse('Email: jesusotteson@mail.weber.edu')
