f699c49083540bdee05a76f9537c09ed8d773322
Jesus Otteson <jesusotteson@mail.weber.edu>
2020-10-06 13:57:01 -0600

    UI Commit

--- PortfolioDatabase/urls.py
+++ PortfolioDatabase/urls.py
@@ -1,9 +1,12 @@
 from . import views
 from django.urls import path
 
+app_name = 'PortfolioDatabase'
 urlpatterns = [
     path('', views.Home, name='Home'),
     path('hobbies/', views.Hobbies, name='Hobbies'),
+    path('<int:hobby_id>', views.hobby_detail, name="hobby_detail"),
     path('portfolio/', views.Portfolio, name='Portfolio'),
+    path('<int:portfolio_id>/portfolio_detail', views.portfolio_detail, name="portfolio_detail"),
     path('contact/', views.Contact, name='Contact')
 ]
--- PortfolioDatabase/views.py
+++ PortfolioDatabase/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Hobby
 from .models import Portfolios
+from django.template import loader
 
 
 # Create your views here.
@@ -12,12 +13,36 @@ def Home(request):
 
 def Hobbies(request):
     hobby_list = Hobby.objects.all()
-    return HttpResponse(hobby_list)
+    template = loader.get_template('hobbies/index.html')
+    context = {
+        'hobby_list': hobby_list,
+    }
+    return render(request, 'hobbies/index.html', context)
+
+
+def hobby_detail(request, hobby_id):
+    hobby = Hobby.objects.get(pk=hobby_id)
+    context = {
+        'hobby': hobby
+    }
+    return render(request, 'hobbies/detail.html', context)
 
 
 def Portfolio(request):
     portfolio_list = Portfolios.objects.all()
-    return HttpResponse(portfolio_list)
+    template = loader.get_template('portfolios/index.html')
+    context = {
+        'portfolio_list': portfolio_list,
+    }
+    return render(request, 'portfolios/index.html', context)
+
+
+def portfolio_detail(request, portfolio_id):
+    portfolio = Portfolios.objects.get(pk=portfolio_id)
+    context = {
+        'portfolio': portfolio
+    }
+    return render(request, 'portfolios/detail.html', context)
 
 
 def Contact(request):
